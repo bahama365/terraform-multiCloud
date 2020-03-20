19722408b160e483bbeeb3507e9494dc1cfb354e
bhalook <chanscombe@icloud.com>
2020-03-20 16:27:26 +0000

    updates variables

---
+++ digitalOcean/variables.tf
@@ -0,0 +1,12 @@
+variable "token" {
+	type	= string
+	default = "210ed9ee50ce5f3b592e50e84c9e5e4afd7cedfc60b2bd81f7bd0a6abcf5c345"
+}
+variable "access_id" {
+	type	= string
+	default = "KFZXWGULWLVUPTSPTJKI"
+}
+variable "secret_key" {
+	type    = string
+	default = "YA9yNvFmEFK414dZRz0fsejoH2w5yh10CXcIr9FubqE"
+}
